4dbe4f31406afe65e3220ef039338326a23eec1d
LorenzoNicco <lorenzoniccolai91@gmail.com>
2024-03-09 15:04:06 +0100

    add srR Notes

---
+++ app/Http/Controllers/Tools/NoteController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Tools;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tools\Note;
+use App\Http\Requests\Tools\StoreNoteRequest;
+use App\Http\Requests\Tools\UpdateNoteRequest;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNoteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Note $note)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Note $note)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNoteRequest $request, Note $note)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Note $note)
+    {
+        //
+    }
+}
---
+++ database/seeders/Tools/NoteSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\Tools;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
